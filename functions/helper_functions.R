c04e8d8dc6d040e5992c5701168610e686e62052
Reehl, Sarah <sarah.reehl@pnnl.gov>
2016-08-25 14:59:04 -0700

    remove unnecessary filtering function

--- functions/helper_functions.R
+++ functions/helper_functions.R
@@ -9,3 +9,26 @@ sums_hist = function(thesums=NULL, xlab="", ylab=""){
   }
   return(p)
 }
+
+output_phyloseq_print_html = function(physeq){
+  HTML(
+    paste(
+      '<p class="phyloseq-print">',
+      paste0(capture.output(print(physeq)), collapse=" <br/> "),
+      "</p>"
+    )
+  )
+}
+
+numericInputRow <- function(inputId, label, value, min = NA, max = NA, step = NA, class="form-control", ...){
+  inputTag <- tags$input(id = inputId, type = "number", value = value, class=class, ...)
+  if (!is.na(min)) 
+    inputTag$attribs$min = min
+  if (!is.na(max)) 
+    inputTag$attribs$max = max
+  if (!is.na(step)) 
+    inputTag$attribs$step = step
+  div(style="display:inline-block",
+      tags$label(label, `for` = inputId), 
+      inputTag)
+}
