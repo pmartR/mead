7e9255693f61fb5a936d848d03b4eaf4c0b13102
Reehl, Sarah <sarah.reehl@pnnl.gov>
2016-12-29 12:58:09 -0800

    cleanup to server and ui scripts

--- ui.R
+++ ui.R
@@ -6,7 +6,7 @@ source("./functions/helper_functions.R")
 shinyUI(navbarPage(title = div(img(src = "Honey_Jar.png", height = 33, width = 22),
                                "mead"),
                    theme = "spacelab.css",
-#---------------------------------------- Load Data Tab ----------------------------------------#                   
+                   #---------------------------------------- Load Data Tab ----------------------------------------#                   
                    tabPanel("Load Data",
                             sidebarLayout(
                               sidebarPanel(
@@ -22,75 +22,76 @@ shinyUI(navbarPage(title = div(img(src = "Honey_Jar.png", height = 33, width = 2
                                 br(),
                                 downloadButton('downloadOTUtable', "Download OTU table")
                               ),
-                            mainPanel(
-                                 plotOutput("library_sizes"),
-                                 uiOutput("plots"),
-                                 h4("Uploaded Metadata View"),
-                                 dataTableOutput("sample_metadata")
+                              mainPanel(
+                                plotOutput("library_sizes"),
+                                uiOutput("plots"),
+                                h4("Uploaded Metadata View"),
+                                dataTableOutput("sample_metadata")
+                              )
                             )
-                          )
-                    ),
-#---------------------------------------- kOverA FIltering Tab ----------------------------------------# 
+                   ),
+                   #---------------------------------------- kOverA FIltering Tab ----------------------------------------# 
                    tabPanel( "kOverA Filtering",
                              fluidPage(
                                fluidRow(
-                              # Sidebar 
-                                sidebarPanel(
-                                  actionButton("actionb_filter", "Execute Filter", icon("filter")),
-                                  h4('kOverA OTU Filtering'),
-                                  fluidRow(
-                                    column(width = 12,
-                                               div(class = "col-md-6",
-                                                   numericInputRow("filter_kOverA_count_threshold", "A: Count value minimum",
-                                                                   value = kovera_A, min = 0, step = 1, class = "col-md-12")), 
-                                               div(class = "col-md-6", uiOutput("filter_ui_kOverA_k"))
-                                    )
-                                  )
-                                ),
-                               # Main Panel
-                                column(
-                                 width = 8, offset = 0, 
-                                 h4("Histograms Before and After Filtering"),
-                                 plotOutput("filter_summary_plot"),
-                                 h4("Data Summaries"),
-                                 fluidRow(
-                                   column(width = 6,
-                                          p("Original"),
-                                          htmlOutput('contents')
-                                   ),
-                                   column(width = 6,
-                                          p("Filtered Data:"),
-                                          htmlOutput('filtered_contents')
+                                 # Sidebar 
+                                 sidebarPanel(
+                                   actionButton("actionb_filter", "Execute Filter", icon("filter")),
+                                   h4('kOverA OTU Filtering'),
+                                   fluidRow(
+                                     column(width = 12,
+                                            div(class = "col-md-6",
+                                                numericInputRow("filter_kOverA_count_threshold", "A: Count value minimum",
+                                                                value = kovera_A, min = 0, step = 1, class = "col-md-12")), 
+                                            div(class = "col-md-6", uiOutput("filter_ui_kOverA_k"))
+                                     )
                                    )
-                                  )
-                                )
+                                 ),
+                                 # Main Panel
+                                 column(
+                                   width = 8, offset = 0, 
+                                   h4("Histograms Before and After Filtering"),
+                                   plotOutput("filter_summary_plot"),
+                                   h4("Data Summaries"),
+                                   fluidRow(
+                                     column(width = 6,
+                                            p("Original"),
+                                            htmlOutput('contents')
+                                     ),
+                                     column(width = 6,
+                                            p("Filtered Data:"),
+                                            htmlOutput('filtered_contents')
+                                     )
+                                   )
+                                 )
                                )
                              )
                    ),
-#---------------------------------------- Alpha Diversity Tab ----------------------------------------# 
+                   #---------------------------------------- Alpha Diversity Tab ----------------------------------------# 
                    tabPanel("Alpha Diversity",
-                                sidebarLayout(
-                                  sidebarPanel(
-                                    h3("Diversity"),
-                                    uiOutput("rich_uix_color"),
-                                    selectInput("dist_measures", label = "Measures", choices = c("Observed", "Chao1", "ACE", "Shannon", "Simpson", "InvSimpson", "Fisher"), multiple = TRUE),
-                                    downloadButton('downloadDiversityImage', "Download plot"),
-                                    h3("Richness"),
-                                    checkboxGroupInput("split", "Split?", choices = c("TRUE", "FALSE"), selected = "TRUE"),
-                                    downloadButton('downloadRichnessEstimates', "Download csv of richness estimates")
-                                    
-                                  ),
-                                  mainPanel(plotOutput("richness_plot", width = "100%"))
-                                )
-                    ),
-#---------------------------------------- Ordination Tab ----------------------------------------# 
-                    tabPanel("Ordination",
-                      sidebarLayout(
-                        sidebarPanel(
-                          downloadButton('downloadOrdinationImage', "Download plot")
-                        ),
-                      mainPanel(plotOutput("ordination_plot",  height = 500, width = 700))
-                      )
-                    )
-
+                            sidebarLayout(
+                              sidebarPanel(
+                                h3("Diversity"),
+                                uiOutput("rich_uix_color"),
+                                selectInput("dist_measures", label = "Measures", choices = c("Observed", "Chao1", "ACE", "Shannon", "Simpson", "InvSimpson", "Fisher"), multiple = TRUE),
+                                downloadButton('downloadDiversityImage', "Download plot"),
+                                h3("Richness"),
+                                checkboxGroupInput("split", "Split?", choices = c("TRUE", "FALSE"), selected = "TRUE"),
+                                downloadButton('downloadRichnessEstimates', "Download csv of richness estimates")
+                                
+                              ),
+                              mainPanel(plotOutput("richness_plot", width = "100%"))
+                            )
+                   ),
+                   #---------------------------------------- Ordination Tab ----------------------------------------# 
+                   tabPanel("Ordination",
+                            sidebarLayout(
+                              sidebarPanel(
+                                downloadButton('downloadOrdinationImage', "Download plot")
+                              ),
+                              mainPanel(plotOutput("ordination_plot",  height = 500, width = 700))
+                            )
+                   )
+                   
 ))
+
