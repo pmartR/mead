c823e413e7e751a6e0dde47baea9d9020b1cb3d3
Reehl, Sarah <sarah.reehl@pnnl.gov>
2017-06-13 08:35:37 -0700

    support for iterative filtering complete. Sarah is bomber these days

--- pmartRversion/server.R
+++ pmartRversion/server.R
@@ -29,11 +29,11 @@ shinyServer(function(input, output) {
     return(pmartRseq::as.rRNAdata(e_data = as.character(input$biom$datapath), f_data = as.character(input$qiime$datapath), edata_cname = "OTU"))
   }) #end rRNAobj
 
-  #--------- somewhere to store the applied filters ---------#
+  #-------- filter history support -----------#
   filters <- reactiveValues(otu = list(), sample = list())
   
   #--------- kovera observer ---------#
-  observeEvent(input$otu_filter_go, {
+  observeEvent(input$otu_filter_go,{
     filters$otu[[input$otu_filter_go]] <- otu_filter_obj()
     })
   
@@ -43,11 +43,13 @@ shinyServer(function(input, output) {
   })
   
   #--------- filter application observer ---------#
+  filtered_rRNA_obj <- NULL
   observe({
     # no kovera filter yet
     if (input$otu_filter_go == 0) {
       filt1 <- rRNAobj()
-    } else{
+    } 
+    if (input$otu_filter_go != 0) {
       # apply k over a filter
       isolate({
         filt1 <- pmartRseq::applyFilt(filter_object = filters$otu[[input$otu_filter_go]],
@@ -59,32 +61,24 @@ shinyServer(function(input, output) {
     
     # no sample filter yet
     if (input$sample_filter_go == 0) {
-      filtered_rRNA_obj <- filt1
+      filtered_rRNA_obj <<- filt1
       return(filtered_rRNA_obj)
+    } else{
+      # apply sample filter
+      isolate({
+        filtered_rRNA_obj <<- pmartRseq::applyFilt(filter_object = filters$sample[[input$sample_filter_go]],
+                                                  omicsData = filt1,
+                                                  upper_lim = input$n)
+        return(filtered_rRNA_obj)
+      })
     }
     
-    # apply sample filter
-    isolate({
-      filtered_rRNA_obj <- pmartRseq::applyFilt(filter_object = filters$sample[[input$sample_filter_go]],
-                                                omicsData = filt1,
-                                                upper_lim = input$n)
-      return(filtered_rRNA_obj)
-    })
+ 
+    
   })
 
-  # filtered_rRNA_obj <- reactive({
-  #   if (values$default == 0) {
-  #     return(rRNAobj())
-  #            }
-  #   else{
-  #     # p <<- pmartRseq::applyFilt(filter_object = otu_filter_obj(),
-  #     #                                omicsData = filtered_rRNA_obj(),
-  #     #                                num_samps = input$filter_kOverA_sample_threshold, upper_lim = input$filter_kOverA_count_threshold)  
-  #       return(p)
-  #     }
-  #   })
-  #-------- filter history support -----------#
-  #filters <- reactiveValues(otu_filt = list(), sample_filt = list())
+
+
   
   #--------- Metadata Object for filtering -----------#
   metadata_obj <- reactive({
@@ -119,11 +113,6 @@ shinyServer(function(input, output) {
   )
   
   #---- Charts for f_meta filtering -------#
-
-  #initialize new_metadata_obj object (REMOVE)
-  # new_metadata_obj <- eventReactive(metadata_obj(), {
-  #   return(metadata_obj()[input$selected_indices + 1, ])
-  # })
   # 
   observeEvent(metadata_obj(), {
     # If the metadata has been loaded, create the charts
@@ -175,122 +164,7 @@ shinyServer(function(input, output) {
   }) 
   # end sample metadata filtering
   
- 
-  #--------- Object for filtering -----------#
-
-  # filtered_rRNA_obj <- eventReactive(input$sample_filter_go,{
-  #   current <<-  pmartRseq::applyFilt(filter_object = sample_filter_obj(),
-  #                                     omicsData = current,
-  #                                     upper_lim = input$n)
-  #   print(str(current))
-  #   current
-  # })
-  
-  # observeEvent(input$otu_filter_go,{
-  #   current <- isolate(filtered_rRNA_obj)
-  #   filtered_rRNA_obj <<- pmartRseq::applyFilt(filter_object = otu_filter_obj(),
-  #                                              omicsData = current,
-  #                                              num_samps = input$filter_kOverA_sample_threshold, upper_lim = input$filter_kOverA_count_threshold)
-  #   print(str(filtered_rRNA_obj))
-  #   return(filtered_rRNA_obj)
-  # })
-  # 
-  # observeEvent(input$sample_filter_go,{
-  #   current <- isolate(filtered_rRNA_obj)
-  #   filtered_rRNA_obj <<- pmartRseq::applyFilt(filter_object = sample_filter_obj(),
-  #                                              omicsData =  current,
-  #                                              upper_lim = input$n)
-  # })
-  # 
-  # filtered_rRNA_obj <- reactive({
-  #     current <<- pmartRseq::applyFilt(filter_object = otu_filter_obj(),
-  #                                                 omicsData = current,
-  #                                                 num_samps = input$filter_kOverA_sample_threshold, upper_lim = input$filter_kOverA_count_threshold)
-  #     current})
-  # 
-  # filtered_rRNA_obj <- reactive({
-  #   current <<- pmartRseq::applyFilt(filter_object = sample_filter_obj(),
-  #                                                           omicsData = current, upper_lim = input$n)
-  #     current})
-  
-  # otu_counter <- eventReactive(input$otu_filter_go,{
-  #   input$otu_filter_go
-  # })
-  # 
-  # sample_counter <- eventReactive(input$sample_filter_go,{
-  #   input$sample_filter_go
-  # })
-  # 
-  # filters$otu_filt[[otu_counter()]] <- otu_filter_obj()
-  # filters$sample_filt[[sample_counter()]] <- sample_filter_obj()
-  # 
-  
-
-  # eventReactive(input$otu_filter_go,{
-  #   print("Happening")
-  #   temp1 <- pmartRseq::applyFilt(filter_object = otu_filter_obj(),
-  #                                omicsData = filtered_rRNA_obj(),
-  #                                num_samps = input$filter_kOverA_sample_threshold, upper_lim = input$filter_kOverA_count_threshold)
-  # }, ignoreNULL = TRUE)
-  
-
-
-  # observeEvent(input$sample_filter_go,{
-  #   temp2 <- pmartRseq::applyFilt(filter_object = sample_filter_obj(), omicsData = filtered_rRNA_obj(), upper_lim = input$n)
-  #   filtered_rRNA_obj <- reactive({
-  #     return(temp2)
-  #   })
-  # }, ignoreNULL = TRUE)
-  
-  
-  #------------ Apply filters on action button -----------#
-  # fix this to include A
-  # filtered_otu_obj <- isolate({
-  #   eventReactive(input$otu_filter_go, {
-  #   validate(
-  #     need(input$filter_kOverA_count_threshold > 0, message = "Please use a filter threshold greater than 0")
-  #   )
-  #   pmartRseq::applyFilt(filter_object = otu_filter_obj(), omicsData = filtered_rRNA_obj(), upper_lim = input$filter_kOverA_count_threshold)
-  # })
-  # })
-  # 
-  # filtered_sample_obj <- eventReactive(input$sample_filter_go, {
-  #     validate(
-  #       need(input$filter_kOverA_count_threshold > 0, message = "Please use a filter threshold greater than 0")
-  #     )
-  #     pmartRseq::applyFilt(filter_object = sample_filter_obj(), omicsData = filtered_rRNA_obj(), min_num = input$n)
-  #   })
-  
-  #if(input$otu_filter_go){
- #   filtered_rRna_obj <- reactiveValues(filtered_otu_obj())
- # }
-  # filtered_rRna_obj <- eventReactive(input$sample_filter_go, {
-  #   validate(
-  #     need(input$n > 0, message = "Please use a filter threshold greater than 0")
-  #   )
-  #   pmartRseq::applyFilt(filter_object = sample_filter_obj(), omicsData = rRNAobj(), upper_lim = as.numeric(input$n))
-  # })
-  # filtered_rRna_obj <- reactive({
-  #   #------- TODO: need to display an error if the rRNA object isn't created! ---------#
-  #   validate(
-  #     need(!(is.null(rRNAobj()$e_data)), message = "rRNA object fail. Check to make sure file paths are correct")
-  #   )
-  #   if (is.null(input$biom)) {
-  #     return(NULL)
-  #   }else{
-  #     results <- rRNAobj()
-  #     return(results)
-  #   }
-  # })
-  
-  # filtered_rRna_obj <- reactive({
-  #   validate(need(!(is.null(biom_obj())), message = "biom object needed"))
-  #   return(pmartRseq::as.rRNAdata(e_data = biom_obj(), f_data = metadata_obj(), edata_cname = "OTU", fdata_cname = names(metadata_obj()[1]))) 
-  #   # If the metadata has been loaded, create the charts
-  #   })
-
-  
-  #--------------- k over a  and library read count filtering  ---------#
+  #--------------- k over a filtering  -----------------#
   kovera_k <- 1
   observeEvent(rRNAobj(), {
     maxSamples = reactive({
@@ -316,9 +190,7 @@ shinyServer(function(input, output) {
     validate(
       need(length(filtered_rRNA_obj) > 0 , message = "Upload data first")
     )
-    
-    print(length(filtered_rRNA_obj))
-      return(pmartRseq::count_based_filter(filtered_rRNA_obj, fn = "ka"))
+    return(pmartRseq::count_based_filter(rRNAobj(), fn = "ka"))
   })
   
   output$read_counts_plot <- renderPlot({
@@ -326,40 +198,33 @@ shinyServer(function(input, output) {
       need( input$filter_kOverA_count_threshold >= 0, message = "Enter a count minimum >= 0"),
       need( input$filter_kOverA_sample_threshold >= 0, message = "Enter a sample minimum >= 0")
     )
-    plot(otu_filter_obj(), min_num = input$filter_kOverA_count_threshold, min_samp = input$filter_kOverA_sample_threshold)
+    if (input$sample_filter_go == 0 & input$otu_filter_go == 0) {
+      plot(otu_filter_obj(), min_num = input$filter_kOverA_count_threshold, min_samp = input$filter_kOverA_sample_threshold)
+    } else{
+      otu_filt_obj <- pmartRseq::count_based_filter(filtered_rRNA_obj, fn = "ka")
+      plot(otu_filt_obj, min_num = input$filter_kOverA_count_threshold, min_samp = input$filter_kOverA_sample_threshold)
+      
+    }
   })
   
   
   #-------------- Library read filtering -----------#
 
   sample_filter_obj <- reactive({
-    if ( input$sample_filter_go == 0) {
-      return(pmartRseq::sample_based_filter(omicsData = rRNAobj(), fn = "sum"))
-    }
-    temp <-  pmartRseq::applyFilt(filter_object = filters$sample[[input$sample_filter_go]],
-                                  omicsData = rRNAobj(),
-                                  upper_lim = input$n)
-    return(pmartRseq::sample_based_filter(omicsData = temp, fn = "sum"))
+    return(pmartRseq::sample_based_filter(omicsData = rRNAobj(), fn = "sum"))
   })
   
+
   output$sample_counts_plot <- renderPlot({
     validate(
       need(input$n >= 0, message = "Enter a count minimum >= 0")
     )
-
-    plot(sample_filter_obj(), min_num = input$n)
+    if (input$sample_filter_go == 0 & input$otu_filter_go == 0) {
+      plot(sample_filter_obj(), min_num = input$n)
+    } else {
+      sample_filt_obj <- pmartRseq::sample_based_filter(omicsData = filtered_rRNA_obj, fn = "sum")
+      plot(sample_filt_obj, min_num = input$n)
+    }
   })
   
-  ################ Community Metrics Tab #################
-  #----------- alpha diversity example ----------#
-  
-  a_div <- reactive({
-    alphaDiv_calc(rRNAobj())
-  })
-  
-  output$plot <- renderPlot({
-    plot(a_div())
-  })
-  
-
 }) #end server
