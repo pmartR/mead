f76d27d62a46f2eef16d532c88c3621a5e468056
Meg Pirrung <meg.pirrung@pnnl.gov>
2017-08-04 12:11:18 -0700

    checking in histogram fixes

--- filterBarWidget/inst/htmlwidgets/filterBarWidget.js
+++ filterBarWidget/inst/htmlwidgets/filterBarWidget.js
@@ -17,121 +17,152 @@ HTMLWidgets.widget({
             Shiny.onInputChange("brushed_on", val.colName);
           }
         }
+
+        // console.log(arr);
         //var ext = d3.extent(arr);
 
-        var formatCount = d3.format(",.0f");
+        // var formatCount = d3.format(",.0f");
+        //
+        // var margin = {top: 10, right: 30, bottom: 30, left: 30},
+        // w = width - margin.left - margin.right,
+        // h = height - margin.top - margin.bottom;
 
-        var margin = {top: 10, right: 30, bottom: 30, left: 30},
-        w = width - margin.left - margin.right,
-        h = height - margin.top - margin.bottom;
+        // d3.select(el).append("p")
+        //   .text(val.colName);
+        // var svg = d3.select(el).append("svg")
+        //   .style("width", width)
+        //   .style("height", height);
 
-        d3.select(el).append("p")
-          .text(val.colName);
-        var svg = d3.select(el).append("svg")
-          .style("width", width)
-          .style("height", height);
+        // var g = svg.append("g")
+        //   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        var g = svg.append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        function onlyUnique(value, index, self) { 
+        function onlyUnique(value, index, self) {
           return self.indexOf(value) === index;
         }
-        
-        var unique = arr.filter( onlyUnique ) ;
-        console.log(unique);
-                
-        var x = d3.scaleBand().rangeRound([0, h])
-          .domain(unique);
-          
-      
-        //var y = d3.scaleLinear().rangeRound([h, 0]);
-        
-        var bins = d3.nest().key(function(unique){return unique;}).entries(arr);
-            // .thresholds(x.ticks(10))
 
-        console.log(bins);
-        
+        var unique = arr.filter( onlyUnique ) ;
+        // console.log(unique);
+
+        // var x = d3.scaleBand().rangeRound([0, h])
+        //   .domain(unique);
+
+
+        // //var y = d3.scaleLinear().rangeRound([h, 0]);
+        //
+        var bins = d3.nest().key(function(unique){return unique;}).entries(arr);
+        //     // .thresholds(x.ticks(10))
+        //
+        // console.log(bins);
+        var categories = [];
+        var counts = [];
+        bins.forEach(function(d, i) {
+          categories[i] = d.key
+          counts[i] = d.values.length
+        });
+
        // var rotate = d3.max(bins.map(function(d) {
        //   return (d.x0.toString()).length;
        // })) > 1;
 
-        var xaxis = d3.axisBottom(x)
-          .ticks(bins.values.length);
-          
+        // var xaxis = d3.axisBottom(x)
+        //   .ticks(bins.values.length);
+        //
+        //
+        // var y = d3.scaleLinear()
+        //     .domain([0, d3.max(bins, function(d) { return d.values.length; })])
+        //     .range([h, 0]);
 
-        var y = d3.scaleLinear()
-            .domain([0, d3.max(bins, function(d) { return d.values.length; })])
-            .range([h, 0]);
+        Highcharts.chart(el, {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: val.colName
+            },
+            xAxis: {
+                categories: categories
+            },
+            credits: {
+                enabled: false
+            },
+            // series: bins
+            series: [{
+                name: 'Value',
+                data: counts
+            }],
+            legend: {
+                enabled: false
+            }
+        });
 
-        var bar = g.selectAll(".bar")
-          .data(bins)
-          .enter().append("g")
-            .attr("class", "bar")
-            .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.values.length) + ")"; });
+        // var bar = g.selectAll(".bar")
+        //   .data(bins)
+        //   .enter().append("g")
+        //     .attr("class", "bar")
+        //     .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.values.length) + ")"; });
+        //
+        // bar.append("rect")
+        //     .attr("x", 1)
+        //     .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
+        //     .attr("width", x(bins[0].x1) - x(bins[0].x0));
+        //     //.attr("height", function(d) { return h - y(d.length); });
+        //
+        // bar.append("text")
+        //     .attr("dy", ".75em")
+        //     .attr("y", 6)
+        //     .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
+        //     .attr("text-anchor", "middle")
+        //     .text(function(d) { return formatCount(d.length); });
+        //
+        // var bandwidth = bins[0].x1 - bins[0].x0;
 
-        bar.append("rect")
-            .attr("x", 1)
-            .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
-            .attr("width", x(bins[0].x1) - x(bins[0].x0));
-            //.attr("height", function(d) { return h - y(d.length); });
-
-        bar.append("text")
-            .attr("dy", ".75em")
-            .attr("y", 6)
-            .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
-            .attr("text-anchor", "middle")
-            .text(function(d) { return formatCount(d.length); });
-
-        var bandwidth = bins[0].x1 - bins[0].x0;
 
 
+      // var brush = d3.brushX()
+      //     .extent([[0, 0], [w, h]])
+      //     .on("end", brushended);
 
-      var brush = d3.brushX()
-          .extent([[0, 0], [w, h]])
-          .on("end", brushended);
-          
-      svg.append("g")
-          .attr("class", "brush")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-          .call(brush);
-        function brushended() {
-          if (!d3.event.sourceEvent) return; // Only transition after input.
-          if (!d3.event.selection) return; // Ignore empty selections.
-          var d0 = d3.event.selection.map(x.invert),
-              d1 = d0.map(function(d) {
-                var dd = Math.round(d * 100);
-                var bw =  Math.round(bandwidth * 100);
-                return (Math.round(dd/bw) * bw) / 100 ;
-              });
-
-          // d1.map(x.invert);
-          // If empty or too small, transition to no brush
-          if (d1[0] >= d1[1]) {
-            d1[0] = d1[0];
-            d1[1] = d1[0];
-          }
-
-          d3.select(this).transition().call(d3.event.target.move, d1.map(x));
-          //return brushed data
-          function checkSelection(arr) {
-            return arr >= d1[0] && arr <= d1[1];
-          }
-          function isInRange(arr) {
-            var indices = [], i;
-            for(i = 0; i < arr.length; i++)
-              if (arr[i] >= d1[0] && arr[i] <= d1[1])
-                indices.push(i);
-            return indices;
-          }
-          if (HTMLWidgets.shinyMode) {
-              Shiny.onInputChange("selected_data", arr);
-              Shiny.onInputChange("selected_data", arr.filter(checkSelection));
-              Shiny.onInputChange("selected_indices", isInRange(arr));
-              Shiny.onInputChange("brushed_on", val.colName);
-          }
-
-        }
+      // svg.append("g")
+      //     .attr("class", "brush")
+      //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      //     .call(brush);
+      //   function brushended() {
+      //     if (!d3.event.sourceEvent) return; // Only transition after input.
+      //     if (!d3.event.selection) return; // Ignore empty selections.
+      //     var d0 = d3.event.selection.map(x.invert),
+      //         d1 = d0.map(function(d) {
+      //           var dd = Math.round(d * 100);
+      //           var bw =  Math.round(bandwidth * 100);
+      //           return (Math.round(dd/bw) * bw) / 100 ;
+      //         });
+      //
+      //     // d1.map(x.invert);
+      //     // If empty or too small, transition to no brush
+      //     if (d1[0] >= d1[1]) {
+      //       d1[0] = d1[0];
+      //       d1[1] = d1[0];
+      //     }
+      //
+      //     d3.select(this).transition().call(d3.event.target.move, d1.map(x));
+      //     //return brushed data
+      //     function checkSelection(arr) {
+      //       return arr >= d1[0] && arr <= d1[1];
+      //     }
+      //     function isInRange(arr) {
+      //       var indices = [], i;
+      //       for(i = 0; i < arr.length; i++)
+      //         if (arr[i] >= d1[0] && arr[i] <= d1[1])
+      //           indices.push(i);
+      //       return indices;
+      //     }
+      //     if (HTMLWidgets.shinyMode) {
+      //         Shiny.onInputChange("selected_data", arr);
+      //         Shiny.onInputChange("selected_data", arr.filter(checkSelection));
+      //         Shiny.onInputChange("selected_indices", isInRange(arr));
+      //         Shiny.onInputChange("brushed_on", val.colName);
+      //     }
+      //
+      //   }
 
         // at this stage the chart always exists
         // get difference in keys
--- filterWidget/inst/htmlwidgets/filterWidget.js
+++ filterWidget/inst/htmlwidgets/filterWidget.js
@@ -17,119 +17,189 @@ HTMLWidgets.widget({
             Shiny.onInputChange("brushed_on", val.colName);
           }
         }
-        var ext = d3.extent(arr);
 
-        var formatCount = d3.format(",.0f");
+        function histogram(data, step) {
+            var histo = {},
+                x,
+                i,
+                array = [];
 
-        var margin = {top: 10, right: 30, bottom: 30, left: 30},
-        w = width - margin.left - margin.right,
-        h = height - margin.top - margin.bottom;
+            // Group down
+            for (i = 0; i < data.length; i++) {
+                x = Math.floor(data[i] / step) * step;
+                if (!histo[x]) {
+                    histo[x] = 0;
+                }
+                histo[x]++;
+            }
 
-        d3.select(el).append("p")
-          .text(val.colName);
-        var svg = d3.select(el).append("svg")
-          .style("width", width)
-          .style("height", height);
+            // Make the histo group into an array
+            for (x in histo) {
+                if (histo.hasOwnProperty((x))) {
+                    array.push([parseFloat(x), histo[x]]);
+                }
+            }
 
-        var g = svg.append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        var x = d3.scaleLinear()
-          .rangeRound([0, w])
-          .domain([ext[0],ext[1]].map(Math.round));
-
-        var bins = d3.histogram()
-            .domain(x.domain())
-            // .thresholds(x.ticks(10))
-            (arr);
-
-        var rotate = d3.max(bins.map(function(d) {
-          return (d.x0.toString()).length;
-        })) > 1;
-
-        var xaxis = d3.axisBottom(x)
-          .ticks(bins.length);
-
-        var y = d3.scaleLinear()
-            .domain([0, d3.max(bins, function(d) { return d.length; })])
-            .range([h, 0]);
-
-        var bar = g.selectAll(".bar")
-          .data(bins)
-          .enter().append("g")
-            .attr("class", "bar")
-            .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
-
-        bar.append("rect")
-            .attr("x", 1)
-            .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
-            // .attr("width", x(bins[0].x1) - x(bins[0].x0))
-            .attr("height", function(d) { return h - y(d.length); });
-
-        bar.append("text")
-            .attr("dy", ".75em")
-            .attr("y", 6)
-            .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
-            .attr("text-anchor", "middle")
-            .text(function(d) { return formatCount(d.length); });
-
-        var bandwidth = bins[0].x1 - bins[0].x0;
-
-        g.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + h + ")")
-            .call(xaxis)
-            .selectAll("text")
-              .attr("y", function(d) { return rotate ? 5 : 10; })
-              .attr("x", function(d) { return rotate ? 9 : 0; })
-              .attr("transform", function(d) { return rotate ? "rotate(45)" : ""; })
-              .style("text-anchor", function(d) { return rotate ? "start" : ""; });
-
-      var brush = d3.brushX()
-          .extent([[0, 0], [w, h]])
-          .on("end", brushended);
-          
-      svg.append("g")
-          .attr("class", "brush")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-          .call(brush);
-        function brushended() {
-          if (!d3.event.sourceEvent) return; // Only transition after input.
-          if (!d3.event.selection) return; // Ignore empty selections.
-          var d0 = d3.event.selection.map(x.invert),
-              d1 = d0.map(function(d) {
-                var dd = Math.round(d * 100);
-                var bw =  Math.round(bandwidth * 100);
-                return (Math.round(dd/bw) * bw) / 100 ;
-              });
-
-          // d1.map(x.invert);
-          // If empty or too small, transition to no brush
-          if (d1[0] >= d1[1]) {
-            d1[0] = d1[0];
-            d1[1] = d1[0];
-          }
-
-          d3.select(this).transition().call(d3.event.target.move, d1.map(x));
-          //return brushed data
-          function checkSelection(arr) {
-            return arr >= d1[0] && arr <= d1[1];
-          }
-          function isInRange(arr) {
-            var indices = [], i;
-            for(i = 0; i < arr.length; i++)
-              if (arr[i] >= d1[0] && arr[i] <= d1[1])
-                indices.push(i);
-            return indices;
-          }
-          if (HTMLWidgets.shinyMode) {
-              Shiny.onInputChange("selected_data", arr);
-              Shiny.onInputChange("selected_data", arr.filter(checkSelection));
-              Shiny.onInputChange("selected_indices", isInRange(arr));
-              Shiny.onInputChange("brushed_on", val.colName);
-          }
-
+            // Finally, sort the array
+            array.sort(function (a, b) {
+                return a[0] - b[0];
+            });
+            console.log(array);
+            return array;
         }
+        // var ext = d3.extent(arr);
+        //
+        // var formatCount = d3.format(",.0f");
+        //
+        // var margin = {top: 10, right: 30, bottom: 30, left: 30},
+        // w = width - margin.left - margin.right,
+        // h = height - margin.top - margin.bottom;
+        //
+        // d3.select(el).append("p")
+        //   .text(val.colName);
+        // var svg = d3.select(el).append("svg")
+        //   .style("width", width)
+        //   .style("height", height);
+        //
+        // var g = svg.append("g")
+        //   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        //
+        // var x = d3.scaleLinear()
+        //   .domain([ext[0],ext[1]].map(Math.round));
+        //
+        // var bins = d3.histogram()
+        //     .domain(x.domain())
+        //     // .thresholds(x.ticks(10))
+        //     (arr);
+        // console.log(bins);
+
+
+        Highcharts.chart(el, {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                align: 'left',
+                text: val.colName,
+                style: {
+                fontSize: '10px',
+                fontWeight: 'bold'
+              }
+            },
+            xAxis: {
+                gridLineWidth: 1
+            },
+            yAxis: [{
+                title: {
+                    text: null
+                }
+            }],
+            series: [{
+                name: 'Histogram',
+                type: 'column',
+                data: histogram(arr, (Math.max(...arr) - Math.min(...arr))/10),
+                pointPadding: 0,
+                groupPadding: 0,
+                pointPlacement: 'between'
+            }],
+            legend: {
+                enabled: false
+            },
+            tooltip: {
+              enabled: false
+            },
+            credits: {
+                enabled: false
+            }
+        });
+
+        // var rotate = d3.max(bins.map(function(d) {
+        //   return (d.x0.toString()).length;
+        // })) > 1;
+        //
+        // var xaxis = d3.axisBottom(x)
+        //   .ticks(bins.length);
+        //
+        // var y = d3.scaleLinear()
+        //     .domain([0, d3.max(bins, function(d) { return d.length; })])
+        //     .range([h, 0]);
+        //
+        // var bar = g.selectAll(".bar")
+        //   .data(bins)
+        //   .enter().append("g")
+        //     .attr("class", "bar")
+        //     .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
+        //
+        // bar.append("rect")
+        //     .attr("x", 1)
+        //     .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
+        //     // .attr("width", x(bins[0].x1) - x(bins[0].x0))
+        //     .attr("height", function(d) { return h - y(d.length); });
+        //
+        // bar.append("text")
+        //     .attr("dy", ".75em")
+        //     .attr("y", 6)
+        //     .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
+        //     .attr("text-anchor", "middle")
+        //     .text(function(d) { return formatCount(d.length); });
+        //
+        // var bandwidth = bins[0].x1 - bins[0].x0;
+        //
+        // g.append("g")
+        //     .attr("class", "axis axis--x")
+        //     .attr("transform", "translate(0," + h + ")")
+        //     .call(xaxis)
+        //     .selectAll("text")
+        //       .attr("y", function(d) { return rotate ? 5 : 10; })
+        //       .attr("x", function(d) { return rotate ? 9 : 0; })
+        //       .attr("transform", function(d) { return rotate ? "rotate(45)" : ""; })
+        //       .style("text-anchor", function(d) { return rotate ? "start" : ""; });
+
+      // var brush = d3.brushX()
+      //     .extent([[0, 0], [w, h]])
+      //     .on("end", brushended);
+      //
+      // svg.append("g")
+      //     .attr("class", "brush")
+      //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      //     .call(brush);
+      //   function brushended() {
+      //     if (!d3.event.sourceEvent) return; // Only transition after input.
+      //     if (!d3.event.selection) return; // Ignore empty selections.
+      //     var d0 = d3.event.selection.map(x.invert),
+      //         d1 = d0.map(function(d) {
+      //           var dd = Math.round(d * 100);
+      //           var bw =  Math.round(bandwidth * 100);
+      //           return (Math.round(dd/bw) * bw) / 100 ;
+      //         });
+      //
+      //     // d1.map(x.invert);
+      //     // If empty or too small, transition to no brush
+      //     if (d1[0] >= d1[1]) {
+      //       d1[0] = d1[0];
+      //       d1[1] = d1[0];
+      //     }
+      //
+      //     d3.select(this).transition().call(d3.event.target.move, d1.map(x));
+      //     //return brushed data
+      //     function checkSelection(arr) {
+      //       return arr >= d1[0] && arr <= d1[1];
+      //     }
+      //     function isInRange(arr) {
+      //       var indices = [], i;
+      //       for(i = 0; i < arr.length; i++)
+      //         if (arr[i] >= d1[0] && arr[i] <= d1[1])
+      //           indices.push(i);
+      //       return indices;
+      //     }
+      //     if (HTMLWidgets.shinyMode) {
+      //         Shiny.onInputChange("selected_data", arr);
+      //         Shiny.onInputChange("selected_data", arr.filter(checkSelection));
+      //         Shiny.onInputChange("selected_indices", isInRange(arr));
+      //         Shiny.onInputChange("brushed_on", val.colName);
+      //     }
+      //
+      //   }
 
         // at this stage the chart always exists
         // get difference in keys
